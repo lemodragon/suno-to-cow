56b9ac0af0a6fd575dcb5de7c46fbdec30addb3c
悱發 <88694954+lemodragon@users.noreply.github.com>
2024-06-21 18:16:09 +0800

    v_2.3.9

--- suno2cow.py
+++ suno2cow.py
@@ -1,6 +1,7 @@
 import plugins
 import requests
 import json
+import time
 from bridge.reply import Reply, ReplyType
 from bridge.context import ContextType
 from channel.chat_message import ChatMessage
@@ -12,7 +13,7 @@ import os
     name="suno2cow",
     desire_priority=8,
     desc="A plugin for music bot to use suno2cow service",
-    version="2.3.0",
+    version="2.3.9",
     author="Fear",
 )
 class suno2cow(Plugin):
@@ -33,18 +34,30 @@ class suno2cow(Plugin):
             self.handlers[Event.ON_HANDLE_CONTEXT] = self.on_handle_context
 
             self.suno2cow_key = self.config["keys"].get("open_ai_api_key", "")
-            self.model = self.config["keys"].get("model", "gpt-4o")
+            self.model = self.config["keys"].get("model", "")
             self.open_ai_api_base = self.config["keys"].get("open_ai_api_base", "https://api.openai.com/v1")
 
-            self.suno2cow_enabled = self.config["suno2cow"].get("enabled", False)
+            self.suno2cow_enabled = self.config["suno2cow"].get("enabled", "")
             self.suno2cow_service = self.config["suno2cow"].get("service", "")
-            self.suno2cow_group = self.config["suno2cow"].get("group", True)
-            self.suno2cow_qa_prefix = self.config["suno2cow"].get("qa_prefix", "唱")
+            self.suno2cow_group = self.config["suno2cow"].get("group", "")
+            self.suno2cow_qa_prefix = self.config["suno2cow"].get("qa_prefix", [])
             self.suno2cow_prompt = self.config["suno2cow"].get("prompt", "")
+
+            self.recent_queries = {}
+            self.cleanup_interval = 60 * 5  # 5分钟清理一次
+            self.last_cleanup_time = time.time()
 
             logger.info("[suno2cow] inited.")
         except Exception as e:
             logger.warn(f"suno2cow init failed: {e}")
+
+    def cleanup_recent_queries(self):
+        current_time = time.time()
+        self.recent_queries = {
+            query: timestamp
+            for query, timestamp in self.recent_queries.items()
+            if current_time - timestamp < self.cleanup_interval
+        }
 
     def on_handle_context(self, e_context: EventContext):
         context = e_context["context"]
@@ -55,49 +68,105 @@ class suno2cow(Plugin):
         content = context.content
         isgroup = e_context["context"].get("isgroup", False)
 
-        if content.startswith(self.suno2cow_qa_prefix) and self.suno2cow_enabled:
-            if isgroup and not self.suno2cow_group:
+        if self.suno2cow_enabled and any(content.startswith(prefix) for prefix in self.suno2cow_qa_prefix):
+            if not isgroup or self.suno2cow_group:
+                matched_prefix = next((prefix for prefix in self.suno2cow_qa_prefix if content.startswith(prefix)), None)
+                if matched_prefix:
+                    self.call_service(content, e_context, matched_prefix)
                 return
-            self.call_service(content, e_context)
+
+    def call_service(self, content, e_context, prefix):
+        # 检查问题是否已经存在于最近的问题列表中
+        if content in self.recent_queries:
+            reply = Reply()
+            reply.type = ReplyType.TEXT
+            reply.content = "已经在拼命制作了..."
+            e_context["reply"] = reply
+            e_context.action = EventAction.BREAK_PASS
             return
 
-    def call_service(self, content, e_context):
+        # 将问题添加到最近的问题列表中
+        self.recent_queries[content] = time.time()
+
+        # 检查是否需要进行清理
+        if time.time() - self.last_cleanup_time > self.cleanup_interval:
+            self.cleanup_recent_queries()
+            self.last_cleanup_time = time.time()
         headers = {
             'Content-Type': 'application/json',
             'Authorization': f'Bearer {self.suno2cow_key}'
         }
-        payload = json.dumps({
+        user_message = content[len(prefix):].strip()
+        payload = {
             "model": self.model,
             "messages": [
                 {"role": "system", "content": self.suno2cow_prompt},
-                {"role": "user", "content": content[len(self.suno2cow_qa_prefix):]}
+                {"role": "user", "content": user_message}
             ]
-        })
-        try:
-            api_url = f"{self.open_ai_api_base}/chat/completions"
-            response = requests.post(api_url, headers=headers, data=payload)
-            response.raise_for_status()
-            response_data = response.json()
-            if "choices" in response_data and len(response_data["choices"]) > 0:
-                first_choice = response_data["choices"][0]
-                if "message" in first_choice and "content" in first_choice["message"]:
-                    response_content = first_choice["message"]["content"].strip()
-                    reply_content = response_content.replace("\\n", "\n")
+        }
+        
+        logger.info(f"Sending payload: {payload}")  # 添加日志输出，调试时可以看到完整的payload
 
-                    # 新增: 删除包含大量🎵符号的行
-                    lines = reply_content.split('\n')
-                    new_lines = [line for line in lines if not line.strip().replace('🎵', '').strip() == '']
-                    reply_content = '\n'.join(new_lines)
-            else:
-                reply_content = "Content not found or error in response"
+        max_retries = 2  # 设置重试次数
+        retry_count = 0
 
-        except requests.exceptions.RequestException as e:
-            logger.error(f"Error calling new combined api: {e}")
-            reply_content = f"An error occurred"
-
+        # 监听到“唱”“奏”关键词时返回特定消息
         reply = Reply()
         reply.type = ReplyType.TEXT
-        reply.content = f"{remove_markdown(reply_content)}"
+        reply.content = "\n正在加急创作中，预计3分钟，请耐心等待...."
+        e_context["reply"] = reply
+        e_context.action = EventAction.BREAK_PASS
+
+        while retry_count < max_retries:
+            try:
+                api_url = f"{self.open_ai_api_base}/chat/completions"
+                response = requests.post(api_url, headers=headers, json=payload)
+                response.raise_for_status()  # 如果响应状态不是200，将抛出异常
+
+                response_data = response.json()
+                if "choices" in response_data and len(response_data["choices"]) > 0:
+                    first_choice = response_data["choices"][0]
+                    if "message" in first_choice and "content" in first_choice["message"]:
+                        response_content = first_choice["message"]["content"].strip()
+                        reply_content = response_content.replace("\\n", "\n")
+
+                        # 新增: 删除包含大量🎵符号的行
+                        lines = reply_content.split('\n')
+                        new_lines = [line for line in lines if not line.strip().replace('🎵', '').strip() == '']
+                        reply_content = '\n'.join(new_lines)
+
+                        reply = Reply()
+                        reply.type = ReplyType.TEXT
+                        reply.content = remove_markdown(reply_content)
+                        e_context["reply"] = reply
+                        e_context.action = EventAction.BREAK_PASS
+                        return
+                    
+                else:
+                    reply_content = "Content not found or error in response"
+
+                break  # 如果请求成功，退出循环
+
+            except requests.exceptions.RequestException as e:
+                logger.error(f"Error calling API: {e}")
+                retry_count += 1
+
+                if retry_count >= max_retries:
+                    logger.error("Max retries exceeded.")
+                    reply = Reply()
+                    reply.type = ReplyType.TEXT
+                    reply.content = "请求失败，请稍后再试。"
+                    e_context["reply"] = reply
+                    e_context.action = EventAction.BREAK_PASS
+                    return
+
+                # 等待3分半钟后重试
+                time.sleep(210)  # 3分半钟，单位为秒
+
+        # 如果所有重试都失败，返回错误消息
+        reply = Reply()
+        reply.type = ReplyType.TEXT
+        reply.content = "创作过程中遇到了一些问题，请稍后再试。"
         e_context["reply"] = reply
         e_context.action = EventAction.BREAK_PASS
 
